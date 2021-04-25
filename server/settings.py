cb996070ec18c09ba35ee237a518a896a557ca90
Lincoln Karuhanga <lincoln.karuhanga@gmail.com>
2021-04-25 19:14:23 +0300

    local to allowed hosts.

--- server/settings.py
+++ server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
