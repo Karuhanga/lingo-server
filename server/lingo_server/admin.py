db1ae8b8207de03278f9da4ed080a52a08de996e
Lincoln Karuhanga <lincoln.karuhanga@gmail.com>
2021-04-25 10:29:28 +0300

    Add indices.

--- server/lingo_server/admin.py
+++ server/lingo_server/admin.py
@@ -15,4 +15,12 @@ class WordAdmin(ModelAdmin):
 
 @register(DictionaryVersion)
 class DictionaryVersionAdmin(ModelAdmin):
-    pass
+    readonly_fields = ('words', 'created_at', 'updated_at')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.words = [
+                word.word for word in Word.objects.order_by('word').filter(language_id=obj.language_id).all()
+            ]
+
+        super().save_model(request, obj, form, change)
--- server/lingo_server/models.py
+++ server/lingo_server/models.py
@@ -2,23 +2,32 @@ from django.db.models import Model, CharField, ForeignKey, JSONField, DateTimeFi
 
 
 class Language(Model):
-    name = CharField("Language Name", max_length=100, unique=True, null=False)
+    name = CharField("Language Name", max_length=100, unique=True, null=False, db_index=True)
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Word(Model):
     class Meta:
         unique_together = ('word', 'language')
 
-    word = CharField(max_length=100, null=False)
-    language = ForeignKey(Language, on_delete=CASCADE)
+    word = CharField(max_length=100, null=False, db_index=True)
+    language = ForeignKey(Language, on_delete=CASCADE, db_index=True)
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.word
 
 
 class DictionaryVersion(Model):
     words = JSONField(null=False)
-    language = ForeignKey(Language, on_delete=CASCADE)
+    language = ForeignKey(Language, on_delete=CASCADE, db_index=True)
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.language}/{self.created_at.strftime('%Y/%m/%d')}-{self.id}"
