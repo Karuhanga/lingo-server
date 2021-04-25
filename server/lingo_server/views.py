ce76dde8b7bfb63f6c07a9766bf593d550f6a001
Lincoln Karuhanga <lincoln.karuhanga@gmail.com>
2021-04-25 12:59:55 +0300

    Squash API bugs.

--- server/lingo_server/views.py
+++ server/lingo_server/views.py
@@ -1,26 +1,30 @@
+import json
+
 from django.http import JsonResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from lingo_server.models import DictionaryVersion, Language, WordSuggestion
 
 
 @require_http_methods(["GET"])
-def dictionary_version_is_latest(request: HttpRequest, dictionary_id: int):
-    dictionary_version = DictionaryVersion.objects.filter(id=dictionary_id)
-    is_latest = DictionaryVersion.objects.filter(language=dictionary_version).order_by('-created_at').first().id == dictionary_id
-    return JsonResponse(dict(data=is_latest))
+def dictionary_version_is_latest(request: HttpRequest, dictionary_version_id: int):
+    dictionary_version = DictionaryVersion.objects.get(id=dictionary_version_id)
+    is_latest = DictionaryVersion.objects.filter(language=dictionary_version.language).order_by('-created_at').first().id == dictionary_version_id
+    return JsonResponse(dict(data=dict(is_latest=is_latest)))
 
 
 @require_http_methods(["GET"])
-def latest_dictionary(request: HttpRequest, language_name: str):
+def latest_dictionary_version(request: HttpRequest, language_name: str):
     dictionary_version = DictionaryVersion.objects.filter(language__name=language_name).order_by('-created_at').first()
     return JsonResponse(dict(data=dictionary_version.to_dict()))
 
 
+@csrf_exempt
 @require_http_methods(["POST"])
 def suggest_word(request: HttpRequest, language_name: str):
-    word = request.POST['word']
-    language = Language.objects.filter(name=language_name).one()
+    word = json.loads(request.body)['word']
+    language = Language.objects.get(name=language_name)
     word, _ = WordSuggestion.objects.get_or_create(language=language, word=word)
 
     return JsonResponse(dict(data=word.to_dict()))
